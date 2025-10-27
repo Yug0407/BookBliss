2cb8bc7c313ced1714b22ad4b497919301335d4f
Yug0407 <yugpadhiyar2006@gmail.com>
2025-10-27 20:17:10 +0530

    Hero Component Complited

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,21 @@
+import { useLocation , Route , Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './Pages/Home';
 
 function App() {
 
+  const isOwnerPath = useLocation().pathname.includes('owner');
 
   return (
-    <>
-      <h1>My Hotel</h1>
-    </>
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
